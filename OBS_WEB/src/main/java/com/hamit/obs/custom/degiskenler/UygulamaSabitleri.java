61daf7340d2acf2a52d27a09c3471c682b210a30
hamit <hamit@okumus.com>
2025-01-14 16:51:16 +0200

    1

--- OBS_WEB/src/main/java/com/hamit/obs/custom/degiskenler/UygulamaSabitleri.java
+++ OBS_WEB/src/main/java/com/hamit/obs/custom/degiskenler/UygulamaSabitleri.java
@@ -1,19 +1,34 @@
 package com.hamit.obs.custom.degiskenler;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+
+import jakarta.annotation.PostConstruct;
 
 @Component
 public class UygulamaSabitleri {
-	
+
 	public static final String CariRaporYeri = "static/raporlar/CariRaporlar";
 	public static final String KambiyoRaporYeri = "static/raporlar/KambiyoRaporlar";
 	public static final String AdresRaporYeri = "static/raporlar/AdresRaporlar";
-	//public static final String forumConnString = "jdbc:postgresql://78.189.76.247:5433/obs_forum" ;
-	//public static final String FORUMUSER_STRING = "hamit" ;
-	//public static final String FORUMPWD_STRING = "oOk271972" ;
-	//public static final String forumConnString = "jdbc:postgresql://dpg-ctv1npqj1k6c73ehb0o0-a.oregon-postgres.render.com:5432/obs_user_2dyb";
-	public static final String forumConnString = "jdbc:postgresql://dpg-ctv1npqj1k6c73ehb0o0-a:5432/obs_user_2dyb";
-	public static final String FORUMUSER_STRING = "obs_user_2dyb_user" ;
-	public static final String FORUMPWD_STRING = "CCzrN1vJCzHcg27tKSlCTwFZliXEM0Pc";
-	
+
+	public static String forumConnString = "";
+	public static String FORUMUSER_STRING = "";
+	public static String FORUMPWD_STRING = "";
+
+	@Value("${spring.datasource.username}")
+	private String forumUser;
+
+	@Value("${spring.datasource.password}")
+	private String forumPassword;
+
+	@Value("${spring.datasource.url}")
+	private String forumUrl;
+
+	@PostConstruct
+	private void init() {
+		FORUMUSER_STRING = forumUser;
+		FORUMPWD_STRING = forumPassword;
+		forumConnString = forumUrl ;
+	}
 }
