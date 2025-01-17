305988853174e9efc2073ebefe9bf234d5d6cbde
hamit <hamit@okumus.com>
2025-01-17 14:20:22 +0200

    1

--- OBS_WEB/src/main/resources/static/obs_js_files/stok/uretim.js
+++ OBS_WEB/src/main/resources/static/obs_js_files/stok/uretim.js
@@ -46,3 +46,53 @@ async function anagrpChanged(anagrpElement) {
 		document.body.style.cursor = "default";
 	}
 }
+
+async function urnaramaYap() {
+	
+	const aramaInput = document.getElementById("girenurnkod").value;
+	if (!aramaInput || aramaInput === "") {
+		return;
+	}
+	document.body.style.cursor = "wait";
+	document.getElementById("errorDiv").style.display = "none";
+	errorDiv.innerText = "";
+	try {
+		const response = await fetchWithSessionCheck("stok/urnArama", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/x-www-form-urlencoded"
+			},
+			body: new URLSearchParams({ arama: aramaInput })
+		});
+		const dto = response;
+		if (dto.errorMessage === "Bu Numarada Urun Yok") {
+			document.getElementById("errorDiv").innerText = dto.errorMessage;
+			return;
+		}
+		document.getElementById("recetekod").value = dto.recete;
+		document.getElementById("adi").innerText = dto.adi;
+		document.getElementById("birim").innerText = dto.birim; 
+		document.getElementById("anagrpl").innerText = dto.anagrup;
+		document.getElementById("altgrpl").innerText = dto.altgrup;
+		document.getElementById("agirlik").innerText = dto.agirlik;
+		document.getElementById("barkod").innerText = dto.barkod;
+		document.getElementById("sinif").innerText = dto.sinif;
+		
+		const imgElement = document.getElementById("resimGoster");
+		if (dto.base64Resim && dto.base64Resim.trim() !== "") {
+			const base64String = 'data:image/jpeg;base64,' + dto.base64Resim.trim();
+			imgElement.src = base64String;
+			imgElement.style.display = "block";
+		} else {
+			imgElement.src = "";
+			imgElement.style.display = "none";
+		}
+		document.getElementById("errorDiv").style.display = "none";
+		document.getElementById("errorDiv").innerText = "";
+	} catch (error) {
+		document.getElementById("errorDiv").style.display = "block";
+		document.getElementById("errorDiv").innerText = error.message || "Beklenmeyen bir hata oluştu.";
+	} finally {
+		document.body.style.cursor = "default";
+	}
+}
