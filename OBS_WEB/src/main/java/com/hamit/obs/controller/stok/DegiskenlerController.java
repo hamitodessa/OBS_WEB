6bf9941a6e6cd420e05698c6ef62559f58537fef
hamit <hamit@okumus.com>
2025-01-23 15:12:59 +0200

    1

--- OBS_WEB/src/main/java/com/hamit/obs/controller/stok/DegiskenlerController.java
+++ OBS_WEB/src/main/java/com/hamit/obs/controller/stok/DegiskenlerController.java
@@ -1,13 +1,113 @@
 package com.hamit.obs.controller.stok;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.hamit.obs.exception.ServiceException;
+import com.hamit.obs.service.fatura.FaturaService;
 
 @Controller
 public class DegiskenlerController {
 
-    @GetMapping("/stok/degiskenler")
-    public String degiskenler() {
-        return "stok/degiskenler";
-    }
+	@Autowired
+	private FaturaService faturaService;
+	
+	@GetMapping("/stok/degiskenler")
+	public Model uretim(Model model) {
+		List<Map<String, Object>> anaKodlari = faturaService.stk_kod_degisken_oku("ANA_GRUP", "AGID_Y", "ANA_GRUP_DEGISKEN") ;
+		model.addAttribute("anaKodlari", (anaKodlari != null) ? anaKodlari : new ArrayList<>());
+		return model;
+	}
+    
+    
+    @GetMapping("stok/anagrpOku")
+	@ResponseBody
+	public Map<String, Object> anagrpOku() {
+		Map<String, Object> response = new HashMap<>();
+		try {
+			List<Map<String, Object>> anaKodlari = faturaService.stk_kod_degisken_oku("ANA_GRUP", "AGID_Y", "ANA_GRUP_DEGISKEN") ;
+			response.put("anagrp", anaKodlari); 
+			response.put("errorMessage", "");
+		} catch (ServiceException e) {
+			response.put("errorMessage", e.getMessage());
+		} catch (Exception e) {
+			response.put("errorMessage", "Hata: " + e.getMessage());
+		}
+		return response;
+	}
+
+    @GetMapping("stok/menseiOku")
+	@ResponseBody
+	public Map<String, Object> menseiOku() {
+		Map<String, Object> response = new HashMap<>();
+		try {
+			List<Map<String, Object>> menseiKodlari = faturaService.stk_kod_degisken_oku("MENSEI", "MEID_Y", "MENSEI_DEGISKEN") ;
+			response.put("mensei", menseiKodlari); 
+			response.put("errorMessage", "");
+		} catch (ServiceException e) {
+			response.put("errorMessage", e.getMessage());
+		} catch (Exception e) {
+			response.put("errorMessage", "Hata: " + e.getMessage());
+		}
+		return response;
+	}
+
+    @GetMapping("stok/depoOku")
+	@ResponseBody
+	public Map<String, Object> depoOku() {
+		Map<String, Object> response = new HashMap<>();
+		try {
+			List<Map<String, Object>> depoKodlari = faturaService.stk_kod_degisken_oku("DEPO", "DPID_Y", "DEPO_DEGISKEN") ;
+			response.put("depo", depoKodlari); 
+			response.put("errorMessage", "");
+		} catch (ServiceException e) {
+			response.put("errorMessage", e.getMessage());
+		} catch (Exception e) {
+			response.put("errorMessage", "Hata: " + e.getMessage());
+		}
+		return response;
+	}
+
+    @GetMapping("stok/oz1Oku")
+ 	@ResponseBody
+ 	public Map<String, Object> oz1Oku() {
+ 		Map<String, Object> response = new HashMap<>();
+ 		try {
+ 			List<Map<String, Object>> oz1OkuKodlari = faturaService.stk_kod_degisken_oku("OZEL_KOD_1", "OZ1ID_Y", "OZ_KOD_1_DEGISKEN") ;
+ 			response.put("oz1", oz1OkuKodlari); 
+ 			response.put("errorMessage", "");
+ 		} catch (ServiceException e) {
+ 			response.put("errorMessage", e.getMessage());
+ 		} catch (Exception e) {
+ 			response.put("errorMessage", "Hata: " + e.getMessage());
+ 		}
+ 		return response;
+ 	}
+    
+    @GetMapping("stok/oz2Oku")
+  	@ResponseBody
+  	public Map<String, Object> oz2Oku() {
+  		Map<String, Object> response = new HashMap<>();
+  		try {
+  			List<Map<String, Object>> oz2OkuKodlari = faturaService.stk_kod_degisken_oku("OZEL_KOD_2", "OZ2ID_Y", "OZ_KOD_2_DEGISKEN") ;
+  			response.put("oz2", oz2OkuKodlari); 
+  			response.put("errorMessage", "");
+  		} catch (ServiceException e) {
+  			response.put("errorMessage", e.getMessage());
+  		} catch (Exception e) {
+  			response.put("errorMessage", "Hata: " + e.getMessage());
+  		}
+  		return response;
+  	}
+
+
+	
 }
---
+++ OBS_WEB/src/main/resources/static/obs_js_files/stok/degiskenler.js
@@ -0,0 +1,313 @@
+
+document.getElementById("arama").value = "";
+anagrpdoldur();
+
+async function degiskenchange(grpElement) {
+	const grup = grpElement.value;
+	const altgrpdiv = document.getElementById("altgrpdiv");
+	document.getElementById("arama").value = "";
+
+	if (grup === "altgrp") {
+		altgrpdiv.style.display = "block";
+		altgrpdoldur()
+	} else {
+		altgrpdiv.style.display = "none";
+	}
+
+	if (grup === "anagrp") {
+		anagrpdoldur();
+	}
+	else if (grup === "mensei") {
+		menseidoldur();
+	}
+	else if (grup === "depo") {
+		depodoldur();
+	}
+	else if (grup === "oz1") {
+		oz1doldur();
+	}
+	else if (grup === "oz2") {
+		oz2doldur();
+	}
+
+}
+
+
+async function anagrpdoldur() {
+	const errorDiv = document.getElementById("errorDiv");
+	errorDiv.style.display = "none";
+	errorDiv.innerText = "";
+	document.body.style.cursor = "wait";
+	try {
+		const response = await fetchWithSessionCheck("stok/anagrpOku");
+		if (response.errorMessage) {
+			throw new Error(response.errorMessage);
+		}
+		const data = response.anagrp;
+		const tableBody = document.getElementById("degiskenTableBody");
+		tableBody.innerHTML = "";
+		tableBody.classList.add("table-row-height");
+		data.forEach((row) => {
+			const tr = document.createElement("tr");
+			tr.innerHTML = `
+				<td style="display: none;">${row.KOD || ""}</td>
+                <td>${row.ANA_GRUP || ""}</td>
+               `;
+			tr.onclick = () => selectValue(row.ANA_GRUP, row.KOD);
+			tableBody.appendChild(tr);
+		});
+
+		const firstRow = tableBody.rows[0];
+		if (firstRow) {
+			const cells = firstRow.cells;
+			selectValue(cells[1].textContent.trim(), cells[0].textContent.trim());
+		}
+
+	} catch (error) {
+		const modalError = document.getElementById("errorDiv");
+		modalError.style.display = "block";
+		modalError.innerText = `Bir hata oluştu: ${error.message}`;
+	} finally {
+		document.body.style.cursor = "default";
+	}
+}
+
+async function menseidoldur() {
+	const errorDiv = document.getElementById("errorDiv");
+	errorDiv.style.display = "none";
+	errorDiv.innerText = "";
+	document.body.style.cursor = "wait";
+	try {
+		const response = await fetchWithSessionCheck("stok/menseiOku");
+		if (response.errorMessage) {
+			throw new Error(response.errorMessage);
+		}
+		console.info(response);
+		const data = response.mensei;
+
+		const tableBody = document.getElementById("degiskenTableBody");
+		tableBody.innerHTML = "";
+		if (data.length === 0) {
+			return;
+		}
+		tableBody.classList.add("table-row-height");
+		data.forEach((row) => {
+			const tr = document.createElement("tr");
+			tr.innerHTML = `
+				<td style="display: none;">${row.KOD || ""}</td>
+                <td>${row.MENSEI || ""}</td>
+               `;
+			tr.onclick = () => selectValue(row.MENSEI, row.KOD);
+			tableBody.appendChild(tr);
+		});
+		const firstRow = tableBody.rows[0];
+		if (firstRow) {
+			const cells = firstRow.cells;
+			selectValue(cells[1].textContent.trim(), cells[0].textContent.trim());
+		}
+	} catch (error) {
+		const modalError = document.getElementById("errorDiv");
+		modalError.style.display = "block";
+		modalError.innerText = `Bir hata oluştu: ${error.message}`;
+	} finally {
+		document.body.style.cursor = "default";
+	}
+}
+
+async function depodoldur() {
+	const errorDiv = document.getElementById("errorDiv");
+	errorDiv.style.display = "none";
+	errorDiv.innerText = "";
+	document.body.style.cursor = "wait";
+	try {
+		const response = await fetchWithSessionCheck("stok/depoOku");
+		if (response.errorMessage) {
+			throw new Error(response.errorMessage);
+		}
+		console.info(response);
+		const data = response.depo;
+
+		const tableBody = document.getElementById("degiskenTableBody");
+		tableBody.innerHTML = "";
+		if (data.length === 0) {
+			return;
+		}
+		tableBody.classList.add("table-row-height");
+		data.forEach((row) => {
+			const tr = document.createElement("tr");
+			tr.innerHTML = `
+				<td style="display: none;">${row.KOD || ""}</td>
+                <td>${row.DEPO || ""}</td>
+               `;
+			tr.onclick = () => selectValue(row.DEPO, row.KOD);
+			tableBody.appendChild(tr);
+		});
+		const firstRow = tableBody.rows[0];
+		if (firstRow) {
+			const cells = firstRow.cells;
+			selectValue(cells[1].textContent.trim(), cells[0].textContent.trim());
+		}
+	} catch (error) {
+		const modalError = document.getElementById("errorDiv");
+		modalError.style.display = "block";
+		modalError.innerText = `Bir hata oluştu: ${error.message}`;
+	} finally {
+		document.body.style.cursor = "default";
+	}
+}
+
+async function oz1doldur() {
+	const errorDiv = document.getElementById("errorDiv");
+	errorDiv.style.display = "none";
+	errorDiv.innerText = "";
+	document.body.style.cursor = "wait";
+	try {
+		const response = await fetchWithSessionCheck("stok/oz1Oku");
+		if (response.errorMessage) {
+			throw new Error(response.errorMessage);
+		}
+		console.info(response);
+		const data = response.oz1;
+
+		const tableBody = document.getElementById("degiskenTableBody");
+		tableBody.innerHTML = "";
+		if (data.length === 0) {
+			return;
+		}
+		tableBody.classList.add("table-row-height");
+		data.forEach((row) => {
+			const tr = document.createElement("tr");
+			tr.innerHTML = `
+				<td style="display: none;">${row.KOD || ""}</td>
+                <td>${row.OZEL_KOD_1 || ""}</td>
+               `;
+			tr.onclick = () => selectValue(row.OZEL_KOD_1, row.KOD);
+			tableBody.appendChild(tr);
+		});
+		const firstRow = tableBody.rows[0];
+		if (firstRow) {
+			const cells = firstRow.cells;
+			selectValue(cells[1].textContent.trim(), cells[0].textContent.trim());
+		}
+	} catch (error) {
+		const modalError = document.getElementById("errorDiv");
+		modalError.style.display = "block";
+		modalError.innerText = `Bir hata oluştu: ${error.message}`;
+	} finally {
+		document.body.style.cursor = "default";
+	}
+}
+
+async function oz2doldur() {
+	const errorDiv = document.getElementById("errorDiv");
+	errorDiv.style.display = "none";
+	errorDiv.innerText = "";
+	document.body.style.cursor = "wait";
+	try {
+		const response = await fetchWithSessionCheck("stok/oz2Oku");
+		if (response.errorMessage) {
+			throw new Error(response.errorMessage);
+		}
+		console.info(response);
+		const data = response.oz2;
+
+		const tableBody = document.getElementById("degiskenTableBody");
+		tableBody.innerHTML = "";
+		if (data.length === 0) {
+			return;
+		}
+		tableBody.classList.add("table-row-height");
+		data.forEach((row) => {
+			const tr = document.createElement("tr");
+			tr.innerHTML = `
+				<td style="display: none;">${row.KOD || ""}</td>
+                <td>${row.OZEL_KOD_2 || ""}</td>
+               `;
+			tr.onclick = () => selectValue(row.OZEL_KOD_2, row.KOD);
+			tableBody.appendChild(tr);
+		});
+		const firstRow = tableBody.rows[0];
+		if (firstRow) {
+			const cells = firstRow.cells;
+			selectValue(cells[1].textContent.trim(), cells[0].textContent.trim());
+		}
+	} catch (error) {
+		const modalError = document.getElementById("errorDiv");
+		modalError.style.display = "block";
+		modalError.innerText = `Bir hata oluştu: ${error.message}`;
+	} finally {
+		document.body.style.cursor = "default";
+	}
+}
+
+function filterTable() {
+	const searchValue = document.getElementById("arama").value.toLowerCase();
+	const rows = document.querySelectorAll("#degiskenTable tbody tr");
+	rows.forEach((row) => {
+		const rowText = Array.from(row.cells)
+			.map((cell) => cell.textContent.toLowerCase())
+			.join(" ");
+		row.style.display = rowText.includes(searchValue) ? "" : "none";
+	});
+}
+
+async function altgrpdoldur() {
+	document.getElementById("arama").value = "";
+
+	const anagrup = document.getElementById("altgrpAna").value;
+	const errorDiv = document.getElementById("errorDiv");
+	errorDiv.style.display = "none";
+	errorDiv.innerText = "";
+	document.body.style.cursor = "wait";
+	try {
+		const response = await fetchWithSessionCheck("stok/altgrup", {
+			method: 'POST',
+			headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+			body: new URLSearchParams({ anagrup: anagrup }),
+		});
+		if (response.errorMessage) {
+			throw new Error(response.errorMessage);
+		}
+		const data = response.altKodlari
+
+		const tableBody = document.getElementById("degiskenTableBody");
+		tableBody.innerHTML = "";
+		if (data.length === 0) {
+
+			return;
+		}
+		tableBody.classList.add("table-row-height");
+		data.forEach((row) => {
+			if (row.ALT_GRUP != "") {
+				const tr = document.createElement("tr");
+				tr.innerHTML = `
+				<td style="display: none;">${row.KOD || ""}</td>
+                <td>${row.ALT_GRUP || ""}</td>
+               `;
+				tr.onclick = () => selectValue(row.ALT_GRUP, row.KOD);
+				tableBody.appendChild(tr);
+			}
+		});
+		const firstRow = tableBody.rows[0];
+		if (firstRow) {
+			const cells = firstRow.cells;
+			selectValue(cells[1].textContent.trim(), cells[0].textContent.trim());
+		}
+	} catch (error) {
+		const modalError = document.getElementById("errorDiv");
+		modalError.style.display = "block";
+		modalError.innerText = `Bir hata oluştu: ${error.message}`;
+	} finally {
+		document.body.style.cursor = "default";
+	}
+}
+
+function selectValue(selectedaciklama, selectedid) {
+	const inputElement = document.getElementById("aciklama");
+	const idacik = document.getElementById("idacik");
+
+	if (inputElement) {
+		inputElement.value = selectedaciklama;
+		idacik.value = selectedid;
+	}
+}
