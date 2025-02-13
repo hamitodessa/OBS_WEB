3b2bddb62db8b9bb600794e0e7be3c11ad2b80ef
hamit <hamit@okumus.com>
2025-02-13 12:37:40 +0300

    13.02

--- OBS_WEB/src/main/resources/static/obs_js_files/wellcome.js
+++ OBS_WEB/src/main/resources/static/obs_js_files/wellcome.js
@@ -16,27 +16,22 @@ async function getWellcomeData() {
 			if (item.modul === "Cari Hesap") {
 				document.getElementById("ckod").innerText = item.progkodu;
 				document.getElementById("cserver").innerText = item.server;
-				document.getElementById("cfadi").innerText = item.firma;
 				document.getElementById("chsql").innerText = item.hangi_sql;
 			} else if (item.modul === "Adres") {
 				document.getElementById("akod").innerText = item.progkodu;
 				document.getElementById("aserver").innerText = item.server;
-				document.getElementById("afadi").innerText = item.firma;
 				document.getElementById("ahsql").innerText = item.hangi_sql;
 			} else if (item.modul === "Kur") {
 				document.getElementById("kkod").innerText = item.progkodu;
 				document.getElementById("kserver").innerText = item.server;
-				document.getElementById("kfadi").innerText = item.firma;
 				document.getElementById("khsql").innerText = item.hangi_sql;
 			} else if (item.modul === "Kambiyo") {
 				document.getElementById("kakod").innerText = item.progkodu;
 				document.getElementById("kaserver").innerText = item.server;
-				document.getElementById("kafadi").innerText = item.firma;
 				document.getElementById("kahsql").innerText = item.hangi_sql;
 			} else if (item.modul === "Fatura") {
 				document.getElementById("fkod").innerText = item.progkodu;
 				document.getElementById("fserver").innerText = item.server;
-				document.getElementById("ffadi").innerText = item.firma;
 				document.getElementById("fhsql").innerText = item.hangi_sql;
 			}
 		});
