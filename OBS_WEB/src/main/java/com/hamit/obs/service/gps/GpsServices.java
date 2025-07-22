04909be705910b029c595228925f5586da77ecac
hamit <hamit@okumus.com>
2025-07-22 22:46:15 +0300

    22.07

--- OBS_WEB/src/main/java/com/hamit/obs/service/gps/GpsServices.java
+++ OBS_WEB/src/main/java/com/hamit/obs/service/gps/GpsServices.java
@@ -3,12 +3,14 @@ package com.hamit.obs.service.gps;
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.commons.net.PrintCommandListener;
 import org.apache.commons.net.ftp.FTPClient;
 import org.apache.commons.net.ftp.FTPFile;
 import org.springframework.stereotype.Service;
@@ -21,10 +23,11 @@ public class GpsServices {
 
 	public List<String> listGpsTxtFiles() throws Exception {
 		FTPClient ftp = new FTPClient();
+		ftp.addProtocolCommandListener(new PrintCommandListener(new PrintWriter(System.out), true));
 		ftp.connect("78.189.76.247");
 		ftp.login("hamitadmin", "SDFks9hfji3#DEd");
+		ftp.enterLocalPassiveMode();
 		ftp.changeWorkingDirectory("GPS");
-
 		FTPFile[] files = ftp.listFiles();
 		List<String> list = new ArrayList<>();
 		for (FTPFile file : files) {
@@ -36,7 +39,6 @@ public class GpsServices {
 		ftp.disconnect();
 		return list;
 	}
-
 	public List<CoordDto> readCoordsFromFtp(String fileName, LocalDate start, LocalDate end) throws Exception {
 		FTPClient ftp = new FTPClient();
 		ftp.connect("78.189.76.247");
